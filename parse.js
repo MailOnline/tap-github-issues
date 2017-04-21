21756da2b6e1c7b8c18e2b3459df9c60a8b9cbe9
Evgeny Poberezkin <e.poberezkin@me.com>
2017-04-21 16:42:39 +0100

    0.1.0

--- parse.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var Parser = require('tap-parser');
-var util = require('util');
-var p = new Parser({passes: true}, function (results) {
-  console.log(util.inspect(results, {depth: null}));
-});
- 
-process.stdin.pipe(p);
